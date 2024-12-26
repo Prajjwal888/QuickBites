7fa86249294dee7a0bf68bd034d2b76d50132c45
Prajjwal888 <chouhanprajjwal8@gmail.com>
2024-12-26 23:03:57 +0530

    third-commit

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -8,7 +8,7 @@ import Add from "./pages/Add/Add";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 const App = () => {
-  const url = "http://localhost:4000";
+  const url = "https://food-delivery-backend-hvf4.onrender.com";
   return (
     <div>
       <ToastContainer></ToastContainer>
